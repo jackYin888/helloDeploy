5139c57c961be4f9e5f94da77ff6ae719dfb7091
JackYin <1259559060@qq.com>
2017-06-26 08:00:18 +0800

    添加express

--- app/index.js
+++ app/index.js
@@ -1,36 +1,30 @@
 var http = require('http')
     , exec = require('exec')
+var express = require('express')
+var app = express()
 
 const PORT = 8091
-    , PATH = '../'
+    , PATH = '..'
 
-var deployServer = http.createServer(function (request, response) {
-    if (request.url.search(/deploy\/?$/i) > 0) {
+app.get('/', function (req, res) {
+    res.send('hello world');
+});
+app.post('/deploy', (req, res) => {
+    var commands = [
+        'git pull',
+    ].join(' && ')
+    exec(commands, function (err, out, code) {
+        if (err instanceof Error) {
 
-        var commands = [
-            'cd ' + PATH,
-            'git pull'
-        ].join(' && ')
-
-        exec(commands, function (err, out, code) {
-            if (err instanceof Error) {
-                response.writeHead(500)
-                response.end('Server Internal Error.')
-                throw err
-            }
-            process.stderr.write(err)
-            process.stdout.write(out)
-            response.writeHead(200)
-            response.end('Deploy Done.')
-
-        })
-
-    } else {
-
-        response.writeHead(404)
-        response.end('Not Found.')
-
-    }
+            res.send('Server Internal Error.')
+            throw err
+        }
+        process.stderr.write(err)
+        process.stdout.write(out)
+        res.send('Deploy Done.')
+    })
 })
 
-deployServer.listen(PORT)
+app.listen(PORT, () => {
+    console.log(`listrning on ${PORT}`)
+})
